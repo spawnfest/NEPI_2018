39d790873efc8a8c20e73ae75815243b4b837918
Arkadiusz Plichta <el.pikel@gmail.com>
2018-11-24 18:30:23 +0100

    socket

--- loca_umbrella/apps/loca_web/lib/loca_web/channels/user_socket.ex
+++ loca_umbrella/apps/loca_web/lib/loca_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule LocaWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", LocaWeb.RoomChannel
+  channel "game:lobby", LocaWeb.GameChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
