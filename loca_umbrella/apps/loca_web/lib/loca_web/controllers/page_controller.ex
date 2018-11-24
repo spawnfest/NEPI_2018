4d754efd8b6d280313fa84f3fe3b08bea2b45ea8
Łukasz Skotarek <lukasz.skotarek@dgc.se>
2018-11-24 17:10:59 +0100

    mix format on page_controller

--- loca_umbrella/apps/loca_web/lib/loca_web/controllers/page_controller.ex
+++ loca_umbrella/apps/loca_web/lib/loca_web/controllers/page_controller.ex
@@ -2,28 +2,29 @@ defmodule LocaWeb.PageController do
   use LocaWeb, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    render(conn, "index.html")
   end
 
   def start_game(conn, params) do
     {:ok, markers, _} = Plug.Conn.read_body(conn)
+
     game_id =
-    Poison.Parser.parse!(markers)
-    |> Loca.GameManager.start_game
-    json conn, %{ game_id: game_id }
+      Poison.Parser.parse!(markers)
+      |> Loca.GameManager.start_game()
+
+    json(conn, %{game_id: game_id})
   end
 
-  def check_position(conn, %{ "game_id" => game_id }) do
+  def check_position(conn, %{"game_id" => game_id}) do
     {:ok, position, _} = Plug.Conn.read_body(conn)
     parsed_position = Poison.Parser.parse!(position)
-    result = Loca.GameManager.check_position(game_id, hd parsed_position)
-    IO.inspect result
-    json conn, %{ status: result }
+    result = Loca.GameManager.check_position(game_id, hd(parsed_position))
+    IO.inspect(result)
+    json(conn, %{status: result})
   end
 
-  def join(conn, %{ "game_id" => game_id }) do
+  def join(conn, %{"game_id" => game_id}) do
     Loca.GameManager.join_game(game_id, "Ziom", %{"lat" => 10, "lng" => 10})
-    render conn, "game.html", game_id: game_id
+    render(conn, "game.html", game_id: game_id)
   end
-
 end
