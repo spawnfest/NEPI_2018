db33116da0769c84bcaaf095573a9363021a9d1b
Maciej Samoraj <maciej.samoraj@gmail.com>
2018-11-24 18:36:33 +0100

    error fix

--- loca_umbrella/apps/loca_web/assets/js/socket.js
+++ loca_umbrella/apps/loca_web/assets/js/socket.js
@@ -53,10 +53,12 @@ let socket = new Socket("/socket", {params: {token: window.userToken}})
 
 socket.connect()
 
-// Now that you are connected, you can join channels with a topic:
-let channel = socket.channel("game:lobby", {})
-channel.join()
-  .receive("ok", resp => { console.log("Joined successfully", resp) })
-  .receive("error", resp => { console.log("Unable to join", resp) })
+// // Now that you are connected, you can join channels with a topic:
+// let channel = socket.channel("game:lobby", {})
+// channel.join()
+//   .receive("ok", resp => { console.log("Joined successfully", resp) })
+//   .receive("error", resp => { console.log("Unable to join", resp) })
+
+window.socket = socket;
 
 export default socket
